b6a21bbc19c0c8c4a40fab06835c076addb2583d
Zayd Tahir <zaydtahir20@gmail.com>
2020-02-28 14:54:27 -0500

    converting turn to angle to new pid in 2020 wpilib

--- src/main/java/frc/robot/commands/drivetrain/TurnToAngleNew.java
+++ src/main/java/frc/robot/commands/drivetrain/TurnToAngleNew.java
@@ -5,18 +5,41 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.command.PIDCommand;
 import frc.robot.Robot;
 import frc.robot.subsystem.DriveTrain;
+import frc.robot.util.RobotMath;
 
 public class TurnToAngleNew extends PIDCommand {
 
-    private static double p, i, d;
+    private static final double kP = 0.015;
+    private static final double kI = 0.000;
+    private static final double kD = 0.00;
+    private double angle;
+
+    private static final double kToleranceDegrees = 2.0f;
 
     private DriveTrain driveTrain;
     private AHRS ahrs;
 
-    public TurnToAngleNew() {
-        super(p, i, d);
+    public TurnToAngleNew(double angle) {
+        super(kP, kI, kD);
         this.driveTrain = Robot.getInstance().getDrive();
         this.ahrs = Robot.getInstance().getNAVX().getAHRS();
+        this.angle = angle;
+    }
+
+    public TurnToAngleNew(final double x, final double y) {
+        super(kP, kI, kD);
+        this.driveTrain = Robot.getInstance().getDrive();
+        this.ahrs = Robot.getInstance().getNAVX().getAHRS();
+        
+        angle = RobotMath.getAngleFromPoint(x, y);
+
+        if (Math.abs(angle - Robot.getInstance().getNAVX().getAHRS().getYaw()) > 90) {
+            if (angle >= 0 && angle <= 180) {
+                angle -= 180;
+            } else {
+                angle += 180;
+            }
+        }
     }
 
     @Override
@@ -24,24 +47,30 @@ public class TurnToAngleNew extends PIDCommand {
         driveTrain.resetEncoders();
 
         driveTrain.setChosenGear(DriveTrain.Gear.FOURTH);
+
+        this.setInputRange(-180.0f, 180.0f);
+        this.setSetpoint(angle);
     }
 
     @Override
     protected double returnPIDInput() {
-        // TODO Auto-generated method stub
-        return 0;
+        return ahrs.getYaw();
     }
 
     @Override
     protected void usePIDOutput(double output) {
-        // TODO Auto-generated method stub
+        if (output < 0) {
+            output = RobotMath.mapDouble(output, -1.0, 0.0, -0.5, -0.2);
+        } else {
+            output = RobotMath.mapDouble(output, 0, 1, 0.2, 0.5);
+        }
 
+        Robot.getInstance().getDrive().arcadeDrive(0, -output);
     }
 
     @Override
     protected boolean isFinished() {
-        // TODO Auto-generated method stub
-        return false;
+        return Math.abs(angle - ahrs.getYaw()) < 2 ;
     }
     
 }
